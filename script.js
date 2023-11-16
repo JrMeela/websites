43dcd2bed0af6d1f03a55ea97af5d07d361e541b
JrMeela <151032027+JrMeela@users.noreply.github.com>
2023-11-16 22:55:07 +0300

    Added new files

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+
+// Function to display a welcome message
+function displayWelcomeMessage() {
+    alert("Welcome to My Portfolio!");
+}
+  
+ // Event listener for document ready
+  document.addEventListener("DOMContentLoaded", function () {
+    // Display a welcome message when the page is loaded
+    displayWelcomeMessage();
+  
+});
+  
